48e344c33b059cbd8a751752d9d1e8e6f89459ef
Dennis Vash <dennisvash@fb.com>
2021-04-14 18:29:18 +0300

    Category and Products types

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,30 @@
 export declare interface IPayloadRegister {
-  name: string | null;
   email: string | null;
+  name: string | null;
   password: string | null;
 }
 
 export declare type IPayloadLogin = Omit<IPayloadRegister, `name`>;
 
 export declare interface IResponseAuth {
-  expiresIn: string;
   accessToken: string;
+  expiresIn: string;
 }
 
 export declare interface IProfile {
   email: string;
   id: string;
+  image: string;
   name: string;
   role: string;
   status: string;
-  image: string;
 }
+
+export declare type Category = {
+  createdAt: string;
+  description: string;
+  id: string;
+  updatedAt: string;
+}
+
+export declare type Categories = Array<Category>;
